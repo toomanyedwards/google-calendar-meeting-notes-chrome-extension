346c3c3d1902c5be37a6a790f0e566746099ffb4
Edward Gemar <edwardgemar@ip-192-168-86-142.ec2.internal>
2020-07-04 11:43:18 -0500

    Added disabled button styles

--- src/AddMeetingNotesButton.js
+++ src/AddMeetingNotesButton.js
@@ -1,14 +1,15 @@
 import React from 'react';
-import styled from 'styled-components'
+import styled, {css} from 'styled-components'
  
 const AddMeetingNotesButton = ({className}) => {
-  return <button id="add_meeting_notes_button" class={className}>Add Meeting Notes</button>
+  return <button id="add_meeting_notes_button" class={className} >Add Meeting Notes</button>
 };
+
 
 const StyledAddMeetingNotesButton = styled(AddMeetingNotesButton)`
     display: inline-block;
-    background-color: rgb(14, 114, 237);
     color: rgb(255, 255, 255);
+    background-color: rgb(14, 114, 237);
     line-height: 36px;
     font-family: "Google Sans", Roboto, Helvetica, Arial, sans-serif;
     font-weight: 500;
@@ -20,10 +21,15 @@ const StyledAddMeetingNotesButton = styled(AddMeetingNotesButton)`
     padding: 0px 16px;
     border-radius: 4px;
 
+
     &:hover {
         background-color: rgb(66, 133, 244);
         cursor: pointer;
       }
+
+    &:disabled {
+        background-color: grey;
+    }
 `;
 
 export default StyledAddMeetingNotesButton;
