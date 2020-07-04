12fea9744ace5cc6941ac1b14f566844fa8c7aa3
Edward Gemar <edwardgemar@ip-192-168-86-142.ec2.internal>
2020-07-03 22:59:07 -0500

    Made add meeting notes button a styled component

--- src/AddMeetingNotesButtonContainer.js
+++ src/AddMeetingNotesButtonContainer.js
@@ -15,17 +15,5 @@ const AddMeetingNotesButtonContainer = ({iconUrl}) =>
         </div>
         <AddMeetingNotesButon/>
     </div>
-
-/*
-const AddMeetingNotesButtonContainer = () => {
-  return 
-    <div class="FrSOzf zoom-video-sec">	
-        <div aria-hidden="true" class="tzcF6">		
-            <span class="DPvwYc zmlogo" aria-hidden="true"></span>	
-        </div>
-        <AddMeetingNotesButon/>
-    </div>
-  
-};*/
  
 export default AddMeetingNotesButtonContainer;
