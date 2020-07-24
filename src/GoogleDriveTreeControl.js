a1f24a3bde3b9343b3dd453116a8aa107c9b5567
Edward Gemar <edwardgemar@ip-192-168-86-160.ec2.internal>
2020-07-24 17:09:11 -0500

    File selection baseline

--- src/GoogleDriveTreeControl.js
+++ src/GoogleDriveTreeControl.js
@@ -6,6 +6,84 @@ import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
 import ChevronRightIcon from "@material-ui/icons/ChevronRight";
 import TreeItem from "@material-ui/lab/TreeItem";
 import { makeStyles } from '@material-ui/core/styles';
+import FolderIcon from '@material-ui/icons/Folder';
+import DocumentIcon from '@material-ui/icons/Description';
+import Typography from "@material-ui/core/Typography";
+
+
+
+const useTreeItemStyles = makeStyles(theme => ({
+  
+    content: {
+      color: theme.palette.text.secondary,
+      borderTopRightRadius: theme.spacing(2),
+      borderBottomRightRadius: theme.spacing(2),
+      paddingRight: theme.spacing(1),
+      fontWeight: theme.typography.fontWeightMedium,
+      "$expanded > &": {
+        fontWeight: theme.typography.fontWeightRegular
+      }
+    },
+   
+    expanded: {},
+    label: {
+      fontWeight: "inherit",
+      color: "inherit"
+    },
+    labelRoot: {
+      display: "flex",
+      alignItems: "center",
+      padding: theme.spacing(0.5, 0)
+    },
+    labelIcon: {
+      marginRight: theme.spacing(1)
+    },
+    labelText: {
+      fontWeight: "inherit",
+      flexGrow: 1
+    }
+  }));
+
+  function StyledTreeItem(props) {
+    const classes = useTreeItemStyles();
+    const {
+      labelText,
+      labelIcon: LabelIcon,
+      labelInfo,
+      color,
+      bgColor,
+      ...other
+    } = props;
+  
+    return (
+      <TreeItem
+        
+        label={
+          <div className={classes.labelRoot}>
+            <LabelIcon color="inherit" className={classes.labelIcon} />
+            <Typography variant="body2" className={classes.labelText}>
+              {labelText}
+            </Typography>
+            <Typography variant="caption" color="inherit">
+              {labelInfo}
+            </Typography>
+          </div>
+        }
+        style={{
+          "--tree-view-color": color,
+          "--tree-view-bg-color": bgColor
+        }}
+        classes={{
+          root: classes.root,
+          content: classes.content,
+          expanded: classes.expanded,
+          group: classes.group,
+          label: classes.label
+        }}
+        {...other}
+      />
+    );
+  }
 
 const { useState } = React;
 
@@ -13,12 +91,14 @@ const ROOT_NODE_ID = "root";
 
 
 const GDTCTreeItem = (props) => {
+    const {labelIcon} = props;
+
+    
     return (
         <TreeItem 
             onMouseEnter={(event)=>{console.log(`onMouseEnter: ${event.relatedTarget.tagName}`)}}
-            
             {...props}
-            
+            label= {labelIcon}        
         />
     );
 }
@@ -78,16 +158,19 @@ const GoogleDriveTreeControl = ({open, onSelectionChanged}) => {
             }
         } else {
             const node = treeData.treeNodeMap.get(nodeId);    
+
+            const icon = node.isFolder?FolderIcon:DocumentIcon;
             
+
             if(node.isFolder && !node.childrenLoaded) {
-                return (<GDTCTreeItem key={nodeId} nodeId={nodeId} label={node.name}><div>Loading...</div></GDTCTreeItem>)
+                return (<StyledTreeItem labelIcon={icon}  key={nodeId} nodeId={nodeId} labelText={node.name}><div>Loading...</div></StyledTreeItem>)
             }
             else {
                 console.log("rendering children")
                 return (
-                    <GDTCTreeItem key={nodeId} nodeId={nodeId} label={node.name}>
+                    <StyledTreeItem labelIcon={icon} key={nodeId} nodeId={nodeId} labelText={node.name}>
                         {Array.isArray(nodeChildrenIds) ? nodeChildrenIds.map((childId) => renderTreeItems(childId)) : null}
-                    </GDTCTreeItem>
+                    </StyledTreeItem>
                 );
             }
         }    
