2c501ca7594c8007b434aa22c5974628b833691b
Edward Gemar <toomanyedwards@gmail.com>
2021-05-28 11:05:56 -0500

    Removed build from .gitignore

---
+++ build/background.js
@@ -0,0 +1,367 @@
+
+const API_KEY = 'AIzaSyBHH78gDxV2O5N_F7ZKxz-E1SNOP82citw';
+const DISCOVERY_DOCS = ["https://www.googleapis.com/discovery/v1/apis/drive/v3/rest"];
+const CLIENT_ID="370393194814-avjjeu46inmjr38cku9312crolphgftt.apps.googleusercontent.com";
+
+const SCOPE=chrome.runtime.getManifest().oauth2.scopes.join(' ');
+const ADD_NOTES_BUTTON_CLICKED_MSG = "ADD_NOTES_BUTTON_CLICKED_MSG";
+
+/**
+ * Initialize google api client
+ */
+
+onGAPILoad = async () => {
+  console.log("Initializing gapi");
+  
+  await gapi.client.init(
+    {
+      apiKey: API_KEY,
+      discoveryDocs: DISCOVERY_DOCS
+    }
+  );
+  console.log("gapi initialized");
+
+  await setGapiToken();
+  
+  //listFiles();
+  
+}
+
+/**
+ * Gets the token for the current chrome user
+ * NOTE: Not guaranteed to google user logged into calendar
+ */
+const getChromeUserToken = () => 
+  new Promise(
+    (resolve) => {
+      chrome.identity.getAuthToken(
+        {interactive: true},
+        (token) => {
+          console.log(`Chrome user token is: ${token}`)
+          resolve(token)
+        }
+      )
+    }
+  );
+
+
+
+/**
+ * Gets the user oauth token
+ */
+const getUserToken = async () => {
+  const token = await getChromeUserToken();
+  console.log(`User token: ${token}`)
+  return token;
+}
+
+/**
+ * Sets the google api client token
+ */
+const setGapiToken = async () => {
+  const token = await getUserToken();
+  console.log(`Setting gapi token to: ${token}`);
+
+  // Set the gapi token
+  gapi.auth.setToken({
+    'access_token': token,
+  });
+
+  return token;
+}
+
+const deleteGoogleDriveFile = async (fileId) => {
+  console.log(`deleteGoogleDriveFile: fileId: ${fileId}`);
+  try {
+    await gapi.client.drive.files.delete({fileId});
+  }
+  catch(resultErrors) {
+    const errors = resultErrors.result.error.errors;
+    console.log(`deleteGoogleDriveFile errors: ${JSON.stringify(errors)}`);
+    throw errors
+  }
+}
+
+const copyNotesDocTemplate = async (meetingNotesTitle, meetingNotesTemplate, meetingNotesFolder) => {
+  console.log(`copyNotesDocTemplate: meetingNotesTitle: ${meetingNotesTitle} meetingNotesTemplate: ${JSON.stringify(meetingNotesTemplate)} meetingNotesFolder: ${JSON.stringify(meetingNotesFolder)}`);
+  
+  try {
+    
+    const result = await gapi.client.drive.files.copy(
+      {
+        fileId: meetingNotesTemplate.id,
+        resource: {
+          name: meetingNotesTitle,
+          parents:[meetingNotesFolder.id]
+        } 
+      }
+    );
+
+    const fileId = result.result.id;
+    
+    console.log(`Created file: ${fileId}`)
+    return fileId;
+  }catch(resultErrors) {
+    const errors = resultErrors.result.error.errors;
+    console.log(`copyNotesDocTemplate errors: ${errors}`);
+
+    if( errors.length === 1 && errors[0].reason === "notFound" ){
+
+      const errorMessage = errors[0].message;
+
+      // Get the id of the folder or file not found
+      // Just extracting the id from the error message since it's not available as a property on
+      // the error :-/
+      const notFoundId = errorMessage.substring(errorMessage.indexOf(": ")+2, errorMessage.length-1);
+
+      if(notFoundId === meetingNotesTemplate.id) {
+          // Set the mesage to an application level message
+        errors[0].originalMessage = errors[0].message;
+        errors[0].message = `Could not find the notes template file: ${meetingNotesTemplate.name}.`;
+      } else if (notFoundId === meetingNotesFolder.id) {
+        // Set the mesage to an application level message
+        errors[0].originalMessage = errors[0].message;
+        errors[0].message = `Could not find the notes destination folder: ${meetingNotesFolder.name}.`;
+      }
+    }
+
+    throw errors;
+  }
+}
+
+
+/**
+ * Handle the add notes button clicked
+ */
+const handleAddNotesButtonClicked = async ({meetingNotesTitle, meetingNotesTemplate, meetingNotesFolder, meetingNotesSharing}) => {
+  console.log(`handleAddNotesButtonClicked: ${meetingNotesTitle}`);
+
+  
+  const token = setGapiToken();
+  var errors = [];
+  var fileId;
+  try{
+    fileId = await copyNotesDocTemplate(meetingNotesTitle, meetingNotesTemplate, meetingNotesFolder);
+  }catch(copyFileErrors) {
+    console.log(`handleAddNotesButtonClicked.copyFileErrors: ${JSON.stringify(copyFileErrors)}`);
+    errors = errors.concat(copyFileErrors);
+    throw errors;
+  }
+
+  try {
+    await setMeetingNotesSharing(fileId, meetingNotesSharing);
+    return fileId;
+  }
+  catch(sharingErrors) {
+    console.log(`handleAddNotesButtonClicked.sharingErrors: ${JSON.stringify(sharingErrors)}`);
+    errors = errors.concat(sharingErrors);
+    try{
+      await deleteGoogleDriveFile(fileId);
+    }catch(fileDeleteErrors) {
+      console.log(`handleAddNotesButtonClicked.fileDeleteErrors: ${JSON.stringify(fileDeleteErrors)}`);
+      errors = errors.concat(fileDeleteErrors);
+     }
+     console.log(`handleAddNotesButtonClicked.errors: ${JSON.stringify(errors)}`);
+     throw errors;
+  }
+}
+
+const setGoogleDriveFileSharingPrivate = async (fileId) => {
+  console.log(`setGoogleDriveFileSharingPrivate ${fileId}`);
+  try{
+    const permissionsResult = await gapi.client.drive.permissions.list({fileId:fileId});
+    
+    // Remove any permissions that aren't owner
+    await Promise.all(
+      permissionsResult.result.permissions.map(
+        async (permission) => {
+          const role = permission.role;
+          if(role != "owner") {
+            await gapi.client.drive.permissions.delete({fileId, id: permission.id});   
+          }
+        }
+      )
+    );  
+    console.log(`setGoogleDriveFileSharingPrivate success!`);
+  }catch(sharingErrors) {
+    const errors = sharingErrors.result.error.errors;
+    console.log(`setGoogleDriveFileSharingPrivate errors: ${JSON.stringify(errors)}`);
+    try{
+      await deleteGoogleDriveFile(fileId);
+    }
+    catch(deleteErrors) {
+      errors.push(deleteErrors.result.error.errors);
+    }
+    throw errors
+  }
+} 
+
+const setGoogleDriveFileSharingDomain = async (fileId, userDomain) => {
+  try{
+    console.log(`setGoogleDriveFileSharingDomain: ${fileId} ${userDomain}`);
+    await setGoogleDriveFilePermissions(
+      fileId,
+      {
+        role: "writer",
+        type: "domain",
+        domain: userDomain,
+        withLink: false,
+        allowFileDiscovery: true
+      }
+    );
+  }catch(errors) {
+    console.log(`setGoogleDriveFileSharingDomain errors: ${errors}`);
+
+    if( errors.length === 1 && errors[0].reason === "insufficientFilePermissions" ){
+      // Set the mesage to an application level message
+      errors[0].originalMessage = errors[0].message;
+      errors[0].message = `You do not have permissions to share this file with the ${userDomain} domain.`;
+    }
+
+    throw errors;
+  }
+}
+
+const setGoogleDriveFileSharingPublic = async (fileId) => {
+  console.log(`setGoogleDriveFileSharingPublic ${fileId}`);
+  try{
+    await setGoogleDriveFilePermissions(
+      fileId,
+      {
+        role: "writer",
+        type: "anyone",
+        withLink: false,
+        allowFileDiscovery: true
+      }
+    );
+  }catch(errors) {
+    console.log(`setGoogleDriveFileSharingPublic errors: ${errors}`);
+
+    if( errors.length === 1 && errors[0].reason === "insufficientFilePermissions" ){
+      // Set the mesage to an application level message
+      errors[0].originalMessage = errors[0].message;
+      errors[0].message = `You do not have permissions to share this file as public.`;
+    }
+
+    throw errors;
+  }
+}
+
+/**
+ * TODO: Use constants for sharing level
+ */
+const setMeetingNotesSharing = async (fileId, meetingNoteSharing) => {
+  switch(meetingNoteSharing.sharingLevel) {
+    case "private":
+      await setGoogleDriveFileSharingPrivate(fileId);
+      break;
+    case "domain":
+      await setGoogleDriveFileSharingDomain(fileId, meetingNoteSharing.userDomain);
+      break;
+    case "public":
+      await setGoogleDriveFileSharingPublic(fileId);
+      break;
+  }
+  
+}
+
+/**
+ * Sets the permissions of a google drive file
+ * 
+ * @param {*} fileId 
+ * @param {*} permissions 
+ */
+const setGoogleDriveFilePermissions = async (fileId, permissions) => {
+  try{
+    await gapi.client.drive.permissions.create(
+      {
+        fileId:fileId,
+        resource: permissions
+      }
+    );
+  }catch(e) {
+    const errors = e.result.error.errors;
+    console.log(`setGoogleDriveFilePermissions errors: ${JSON.stringify(errors)}`);
+    throw errors;
+  }  
+
+}
+
+/**
+ * Listen for messages from content script
+ */
+chrome.extension.onMessage.addListener(
+  (message, sender, sendResponse) => {
+    console.log(`Message received: ${JSON.stringify(message)}`);
+     // setGapiToken();
+  
+    switch(message.type) {
+      case "addNotes":
+        handleAddNotesButtonClicked(message).
+          then( 
+            (fileId)=>{
+              console.log(`handleAddNotesButtonClicked 1: ${fileId}`);
+              sendResponse({meetingNotesDocUrl: getGoogleDocUrlForId(fileId)});
+              console.log(`handleAddNotesButtonClicked 2`);
+            }
+          ).catch(
+            (errors) => {
+              console.log(`handleAddNotesButtonClicked 2 errors: ${JSON.stringify(errors)}`);
+              sendResponse({errors});
+            }
+          );
+  
+        break;
+      case "listGoogleDrive":
+        listGoogleDrive(message.listParams). 
+            then(
+              (filesList) => {
+                console.log(`listGoogleDrive 1: ${filesList}`);
+                sendResponse({filesList});
+              }
+            ).catch(
+              (errors) => {
+                console.log(`listGoogleDrive errors: ${JSON.stringify(errors)}`);
+                sendResponse({errors});
+              } 
+            );
+
+    }
+    
+    return true;
+
+  }
+);
+
+const getGoogleDocUrlForId = (googleFileId) => {
+  return `https://docs.google.com/document/d/${googleFileId}/edit?usp=sharing`
+}
+
+const listGoogleDrive = async (listParams) => {
+  
+    console.log(`listGoogleDrive: listingFiles`);
+    var filesList = [];
+    try{
+      do {
+        
+        const response = await gapi.client.drive.files.list(listParams);
+        console.log(`listGoogleDrive: Page: ${JSON.stringify(response)}`);
+        
+        listParams.pageToken = response.result.nextPageToken;
+        filesList = filesList.concat(response.result.files);
+      }while(listParams.pageToken)
+      
+
+      console.log(`listGoogleDrive: ${JSON.stringify(filesList)}`);
+
+      return filesList;
+    }
+    catch(e) {
+      const errors = e.result.error.errors;
+      console.log(`listGoogleDrive errors: ${JSON.stringify(errors)}`);
+      throw errors;
+    }
+    console.log(`listGoogleDrive: listingFiles`);
+   
+  }
+   
---
+++ build/precache-manifest.a2b9a2b111b9a1807182221637a874f3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "93bc22e3ddaf58fb2f3accca0651c61d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e7e3a54e0d065d27013a",
+    "url": "/static/css/main.chunk.css"
+  },
+  {
+    "revision": "5c5158ba1a13eddaf6c1",
+    "url": "/static/js/2.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e7e3a54e0d065d27013a",
+    "url": "/static/js/main.chunk.js"
+  },
+  {
+    "revision": "7b29681760370a34d4a9",
+    "url": "/static/js/runtime-main.js"
+  }
+]);
